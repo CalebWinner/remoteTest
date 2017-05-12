f0472a1a08d89d1b092edb52b1c2c8b7c2be365f
Caleb Winner <winnerc2@rhs.msu.edu>
2017-05-12 15:52:35 -0400

    initial commit

---
+++ datafetch.php
@@ -0,0 +1,33 @@
+<?php
+print "<script> console.log('Included datafetch.php'); </script>";
+
+$con = mysqli_connect("127.0.0.1","caleb","pass", "db1");
+
+if (!con) {
+  echo "<script>console.log('unable to connect');</script>";
+  die('Could not connect: '. $con->connect_error;
+}
+
+echo "<script>console.log('unable to connect');</script>";
+echo "<script>console.log('connected');</script>";
+echo "<script>console.log('unable to connect');</script>";
+#mysqli_select_db("db1",$con);
+
+$query = "SELECT * FROM pet";
+
+$query_res = $con->query($query);
+echo "<script>console.log('$query_res');</script>";
+if ($query_res->num_rows > 0) {
+  while($row = $query_res->fetch_assoc()) {
+    echo $query_res->fetch_assoc;
+    echo "name: ". $row['name']. " - Species: " . $row['species'];
+  }
+} else {
+  echo "0 results";
+}
+#echo "<p>$query_res</p>";
+
+$con->close();
+
+
+?>
---
+++ testpage.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>TEST</title>
+  </head>
+  <body>
+    <h1>test page title</h1>
+
+    <?php
+    $test = 20;
+    print $test;
+    include('include.php');
+    include('datafetch.php');
+    ?>
+
+
+  </body>
+</html>
